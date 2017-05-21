c5648ab9cc2db6f08fe5584eb5f30e5f4276a3c4
Greg Wilson <gvwilson@third-bit.com>
2017-05-21 11:07:31 -0400

    Word count script

---
+++ bin/wc.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const marked = require('marked')
+
+const countText = (tokens) => {
+  if (Array.isArray(tokens)) {
+    return tokens.reduce((accum, val) => { return accum + countText(val)}, 0)
+  }
+  else if ((typeof tokens == 'object') && (tokens.type == 'text')) {
+    return tokens.text
+      .split(' ')
+      .filter((word) => { return word.length > 0 })
+      .length
+  }
+  else {
+    return 0
+  }
+}
+
+console.log(process.argv.slice(2).reduce((accum, filename) => {
+  const data = fs.readFileSync(filename, 'utf-8')
+  const tokens = new marked.Lexer().lex(data)
+  return accum + countText(tokens)
+}, 0))
