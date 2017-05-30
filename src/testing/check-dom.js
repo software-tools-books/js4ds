897c64159bbfaced11446f86cd50a077365a325a
Greg Wilson <greg.wilson@rangle.io>
2017-05-30 14:01:29 -0400

    Using Cheerio (but broken test)

--- src/testing/check-dom.js
+++ src/testing/check-dom.js
@@ -1,5 +1,6 @@
 const test = require('tape')
 const request = require('supertest')
+const cheerio = require('cheerio')
 const server = require('./server')
 
 test('Home page is HTML with expected title', (t) => {
@@ -8,8 +9,10 @@ test('Home page is HTML with expected title', (t) => {
     .expect('Content-Type', /html/)
     .expect(200)
     .end((err, res) => {
-      const dom = htmlparser.parseDOM(res.text)
-      console.log('dom is', dom)
+      const tree = cheerio.load(res.text)
+      const h1 = tree('h1')
+      t.ok(h1.length === 1, 'Exactly one h1 heading')
+      t.ok(h1[0].text === 'Home', 'Correct heading text')
       t.end()
     })
 })
