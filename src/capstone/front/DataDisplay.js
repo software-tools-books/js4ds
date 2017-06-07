029227a77bf1af5aff48508c0b877156b21329a4
Greg Wilson <greg.wilson@rangle.io>
2017-06-07 11:57:00 -0400

    Using Recharts - will need to rework viz material

--- src/capstone/front/DataDisplay.js
+++ src/capstone/front/DataDisplay.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import ChartistBar from 'react-chartist'
+import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'Recharts'
 
 const DataDisplay = ({data}) => {
   if (data === null) {
     return (<span>---</span>)
   }
-  const cleaned = data
-    .map((record) => {return {x: record.weight, y: record.hindfoot_length}})
-    .filter((record) => {return (record.x !== null) && (record.y !== null)})
-  const series = [cleaned]
-  console.log('cleaned is', cleaned)
+
+  const temp = [{x: 100, y: 200, z: 200}, {x: 120, y: 100, z: 260},
+                {x: 170, y: 300, z: 400}, {x: 140, y: 250, z: 280},
+                {x: 150, y: 400, z: 500}, {x: 110, y: 280, z: 200}]
   return (
-    <ChartistBar data={{series: cleaned}} type={'Line'} />
-  )
+    <ScatterChart width={400} height={400} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+      <XAxis dataKey={'x'} name='stature' unit='cm'/>
+      <YAxis dataKey={'y'} name='weight' unit='kg'/>
+      <Scatter name='A school' data={temp} fill='#8884d8'/>
+      <CartesianGrid />
+      <Tooltip cursor={{strokeDasharray: '3 3'}}/>
+    </ScatterChart>)
 }
 
 export default DataDisplay
