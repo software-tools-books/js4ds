fc7edcc562e33640538c985119fccb700ec7f753
Greg Wilson <greg.wilson@rangle.io>
2017-06-07 13:19:24 -0400

    Scatter plot (with bugs)

--- src/capstone/front/DataDisplay.js
+++ src/capstone/front/DataDisplay.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'Recharts'
 
+const orderPoints = (a, b) => {
+  if (a.x < b.x) {
+    return -1
+  }
+  else if (a.x > b.x) {
+    return 1
+  }
+  else if (a.y < b.y) {
+    return -1
+  }
+  else if (a.y > b.y) {
+    return 1
+  }
+  else {
+    return 0
+  }
+}
+
 const DataDisplay = ({data}) => {
   if (data === null) {
     return (<span>---</span>)
   }
-
-  const temp = [{x: 100, y: 200, z: 200}, {x: 120, y: 100, z: 260},
-                {x: 170, y: 300, z: 400}, {x: 140, y: 250, z: 280},
-                {x: 150, y: 400, z: 500}, {x: 110, y: 280, z: 200}]
+  const display = data
+        .map((rec) => {
+          return {x: rec.hindfoot_length, y: rec.weight}
+        })
+        .filter((rec) => {
+          return (rec.x !== null) && (rec.y !== null)
+        })
+        .sort(orderPoints)
+  console.log('min x', Math.min.apply(null, display.map((rec) => {return rec.x})),
+              'max x', Math.max.apply(null, display.map((rec) => {return rec.x})))
   return (
-    <ScatterChart width={400} height={400} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
-      <XAxis dataKey={'x'} name='stature' unit='cm'/>
+    <ScatterChart width={600} height={600} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+      <XAxis dataKey={'x'} name='hindfoot length' unit='cm'/>
       <YAxis dataKey={'y'} name='weight' unit='kg'/>
-      <Scatter name='A school' data={temp} fill='#8884d8'/>
+      <Scatter name='Creatures' data={display} fill='#8884d8'/>
       <CartesianGrid />
       <Tooltip cursor={{strokeDasharray: '3 3'}}/>
     </ScatterChart>)
