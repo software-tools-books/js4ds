fa53d49aed55af08c4e4778012ac74206a7b5e12
Greg Wilson <greg.wilson@rangle.io>
2017-06-14 11:17:15 -0400

    A working D3 example

---
+++ src/viz/BarChart.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import { scaleLinear } from 'd3-scale'
+import { max, sum } from 'd3-array'
+import { select } from 'd3-selection'
+import { legendColor } from 'd3-svg-legend'
+import { transition } from 'd3-transition'
+
+class BarChart extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount = () => {
+    this.createBarChart()
+  }
+
+  componentDidUpdate = () => {
+    this.createBarChart()
+  }
+
+  createBarChart = () => {
+    const node = this.node
+    const dataMax = max(this.props.data.map(d => sum(d.data)))
+    const barWidth = this.props.size[0] / this.props.data.length
+
+    const legend = legendColor()
+      .scale(this.props.colorScale)
+      .labels(["Wave 1", "Wave 2", "Wave 3", "Wave 4"])
+
+    select(node)
+      .selectAll("g.legend")
+      .data([0])
+      .enter()
+      .append("g")
+      .attr("class", "legend")
+      .call(legend)
+
+    select(node)
+      .select("g.legend")
+      .attr("transform", "translate(" + (this.props.size[0] - 100) + ", 20)")
+
+    const yScale = scaleLinear()
+      .domain([0, dataMax])
+      .range([0, this.props.size[1]])
+
+    select(node)
+      .selectAll("rect.bar")
+      .data(this.props.data)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+
+    select(node)
+      .selectAll("rect.bar")
+      .data(this.props.data)
+      .exit()
+      .remove()
+
+    select(node)
+      .selectAll("rect.bar")
+      .data(this.props.data)
+      .attr("x", (d,i) => i * barWidth)
+      .attr("y", d => this.props.size[1] - yScale(sum(d.data)))
+      .attr("height", d => yScale(sum(d.data)))
+      .attr("width", barWidth)
+      .style("fill", (d,i) => this.props.colorScale(d.launchday))
+      .style("stroke", "black")
+      .style("stroke-opacity", 0.25)
+  }
+
+  render() {
+    return <svg ref={node => this.node = node} width={this.props.size[0]} height={this.props.size[1]}>
+    </svg>
+  }
+}
+
+export default BarChart
--- src/viz/app.js
+++ src/viz/app.js
@@ -1,20 +1,48 @@
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import React from 'react'
-import PlainLines from './PlainLines'
+import BarChart from './BarChart'
+import worlddata from './world'
+import { range } from 'd3-array'
+import { scaleThreshold } from 'd3-scale'
+import { geoCentroid } from 'd3-geo'
 
-const data = [
-  {x: 0,   y: 60},
-  {x: 50,  y: 30},
-  {x: 100, y: 10},
-  {x: 150, y: 50},
-  {x: 200, y: 275},
-  {x: 250, y: 375}
-]
+let appdata = worlddata.features
+  .filter(d => geoCentroid(d)[0] < -20)
+
+appdata
+  .forEach((d,i) => {
+    const offset = Math.random()
+    d.launchday = i
+    d.data = range(30).map((p,q) => q < i ? 0 : Math.random() * 2 + offset)
+  })
+
+const colorScale = scaleThreshold().domain([5,10,20,30]).range(["#75739F", "#5EAFC6", "#41A368", "#93C464"])
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = { screenWidth: 1000, screenHeight: 500 }
+  }
+
+  onResize = () => {
+    this.setState({ screenWidth: window.innerWidth, screenHeight: window.innerHeight - 120 })
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false)
+    this.onResize()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BarChart colorScale={colorScale} data={appdata} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]} />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <div>
-    <h2>Plain Lines</h2>
-    <PlainLines data={data} />
-  </div>,
-  document.getElementById(('root'))
-)
+  <App />,
+  document.getElementById('root')
+);
