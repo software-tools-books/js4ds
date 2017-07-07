7eea00008afc84ace1a034bad5b4772bf0b77811
Greg Wilson <gvwilson@third-bit.com>
2017-07-07 16:57:07 -0400

    Adding search box

--- js/site.js
+++ js/site.js
@@ -1,7 +1,37 @@
 // Add 'data-toggle' to every 'div' with a 'title' attribute.
-// Bootstrap tooltips require both a 'title' and a 'data-toggle' attribute to work.
+// ---
+// Bootstrap tooltips require both a 'title' and a 'data-toggle'
+// attribute to work, but requiring people to type that into Markdown
+// is a pain.
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('div[title]').forEach((node) => {
     node.setAttribute('data-toggle', 'tooltip')
   })
 }, false)
+
+// Handle searches.
+// ---
+// Relies on document having 'meta' element with name 'search-domain'
+// and content equal to the GitHub Pages domain in the _config.yml
+// file, and on the search form having the ID 'search-term'.
+function doSearch() {
+  const queryElement = document.getElementById('search-term')
+  if (! queryElement) {
+    return false
+  }
+  const queryString = queryElement.value
+  if (! queryString) {
+    return false
+  }
+  const searchDomainElement = document.querySelector('meta[name=search-domain]')
+  if (! searchDomainElement) {
+    return false
+  }
+  const searchDomain = searchDomainElement.getAttribute('content')
+  if (! searchDomain) {
+    return false
+  }
+  const url = `https://duckduckgo.com/?q=site%3A${searchDomain}+${encodeURI(queryString)}`
+  window.open(url)
+  return false
+}
