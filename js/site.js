8245b1a2bca66d300d03fbfda81df65eb0be38f8
Greg Wilson <gvwilson@third-bit.com>
2017-07-11 06:52:50 -0400

    Striping tables

--- js/site.js
+++ js/site.js
@@ -1,19 +1,29 @@
-// Add 'data-toggle' to every 'div' with a 'title' attribute.
+// Tidy up page elements.
 // ---
-// Bootstrap tooltips require both a 'title' and a 'data-toggle'
-// attribute to work, but requiring people to type that into Markdown
-// is a pain.
+//
+// 1. Add 'data-toggle' to every 'div' with a 'title' attribute.
+//    Bootstrap tooltips require both a 'title' and a 'data-toggle'
+//    attribute to work, but requiring people to type that into
+//    Markdown is a pain.
+//
+// 2. Make all tables striped by default.
+//
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('div[title]').forEach((node) => {
     node.setAttribute('data-toggle', 'tooltip')
+  })
+  document.querySelectorAll('table').forEach((node) => {
+    node.className += ' table table-striped'
   })
 }, false)
 
 // Handle searches.
 // ---
+//
 // Relies on document having 'meta' element with name 'search-domain'
 // and content equal to the GitHub Pages domain in the _config.yml
 // file, and on the search form having the ID 'search-term'.
+//
 function doSearch() {
   const queryElement = document.getElementById('search-term')
   if (! queryElement) {
