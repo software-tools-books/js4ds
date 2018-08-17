d2f844ddd387b1e565d2633bcf4539d929900107
Greg Wilson <gvwilson@third-bit.com>
2018-08-17 08:55:32 -0400

    Visualization now displays, but: -   As soon as app.js is given type text/babel, the console reports that 'spec' cannot be found.

--- src/viz/react-01/app.js
+++ src/viz/react-01/app.js
@@ -1,9 +1,13 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const VegaLite = require('react-vega-lite')
-
 const spec = {
+  "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
   "description": "A simple bar chart with embedded data.",
+  "data": {
+    "values": [
+      {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
+      {"a": "D","b": 19}, {"a": "E","b": 40}, {"a": "F","b": 34},
+      {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
+    ]
+  },
   "mark": "bar",
   "encoding": {
     "x": {"field": "a", "type": "ordinal"},
@@ -11,10 +15,10 @@ const spec = {
   }
 }
 
-const barData = {
-  "values": [
-    {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
-    {"a": "D","b": 19}, {"a": "E","b": 40}, {"a": "F","b": 34},
-    {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
-  ]
+const options = {
+  "actions": {
+    "export": false,
+    "source": false,
+    "editor": false
+  }
 }
