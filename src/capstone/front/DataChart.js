d4d3534c672632fe6f5bf14991d387bcb50a25bf
stamper <tbyhdgs@gmail.com>
2018-09-01 08:13:22 +0100

    tidy up DataChart.js

--- src/capstone/front/DataChart.js
+++ src/capstone/front/DataChart.js
@@ -5,7 +5,7 @@ import VegaLite from 'react-vega-lite'
 const DataChart = ({data}) => {
   if (! data) {
     return (<p>no data</p>)
-} else {
+  } else {
     const xy_values = data.map((rec) => {
         return {x: rec.hindfoot_avg, y: rec.weight_avg}
     })
@@ -29,35 +29,10 @@ const DataChart = ({data}) => {
         "values": xy_values
     }
     console.log(scatterData)
-  return(
+    return(
       <VegaLite spec={spec} data={scatterData} options={options}/>
-  )
-}
-/*
-  data = {
-    datasets: [
-      {
-        label: 'Weight vs. Hindfoot',
-        fill: false,
-        showLine: false,
-        borderColor: 'rgba(75,192,192,1)',
-        backgroundColor: 'rgba(255,255,255,1)',
-        pointBorderWidth: 2,
-        pointRadius: 5,
-        data: data.map((rec) => {return {x: rec.hindfoot_avg, y: rec.weight_avg}})
-      }
-    ]
+    )
   }
-  const options = {
-    width: 600,
-    height: 600,
-    responsive: false,
-    maintainAspectRatio: false
-  }
-  return (
-    <Scatter data={data} options={options}/>
-  )
-*/
 }
 
 export default DataChart
