5d7355403ab2befcdf716366bc35969b83ea7aa9
Greg Wilson <gvwilson@third-bit.com>
2018-10-27 06:59:50 -0400

    Merging

--- js/youbou.js
+++ js/youbou.js
@@ -11,24 +11,20 @@ const stripeTables = () => {
   const tables = document.querySelectorAll('table')
   Array.from(tables).forEach(t => {
     t.classList.add('table', 'table-striped')
-    console.log(`converting table classlList is now ${t.classList}`)
   })
 }
 
 const fixSpecialLinks = (prefix) => {
   const links = document.querySelectorAll('a')
-  console.log(`links are ${links}`)
   Array.from(document.querySelectorAll('a')).forEach(a => {
     const target = a.getAttribute('href')
     if (target.startsWith('#b:')) {
       a.setAttribute('href', prefix + '/bib/' + target)
       a.classList.add('bibref')
-      console.log(`convert bib item now`, a)
     }
     else if (target.startsWith('#g:')) {
       a.setAttribute('href', prefix + '/gloss/' + target)
       a.classList.add('glossref')
-      console.log(`convert gloss item now`, a)
     }
   })
 }
