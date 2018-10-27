32212a694da0d5cc8281e780d29b2ce9f73c797e
Greg Wilson <gvwilson@third-bit.com>
2018-10-27 06:56:44 -0400

    Simplifying

--- temp/example.js
+++ temp/example.js
@@ -1,6 +1,6 @@
 (function() {
-  const container = document.querySelectorAll('div.toc')[0] // '[0]' to get first element
-  const headings = [...document.querySelectorAll('h2')] // [...call()] to convert NodeList to array
+  const container = document.querySelectorAll('div.toc')[0]
+  const headings = Array.from(document.querySelectorAll('h2'))
   const items = headings
         .map((h) => '<li><a href="#' + h.id + '">' + h.innerHTML + '</a></li>')
         .join('\n')
