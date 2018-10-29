7c5b763f3e49ea03161387b604f57bfca909ac85
Greg Wilson <gvwilson@third-bit.com>
2018-10-29 08:18:51 -0400

    No longer need this temporary example

--- temp/example.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  const container = document.querySelectorAll('div.toc')[0]
-  const headings = Array.from(document.querySelectorAll('h2'))
-  const items = headings
-        .map((h) => '<li><a href="#' + h.id + '">' + h.innerHTML + '</a></li>')
-        .join('\n')
-  container.innerHTML = '<ul>\n' + items + '</ul>'
-})()
