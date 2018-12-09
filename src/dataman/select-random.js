82a5cf1d6a63f60902a93604034d9f4668d04305
stamper <tbyhdgs@gmail.com>
2018-12-09 14:41:50 +0100

    simpler random sort, without ternary operator

--- src/dataman/select-random.js
+++ src/dataman/select-random.js
@@ -6,7 +6,7 @@ const lines = fs.readFileSync(inputFile, 'utf-8')
 header = lines[0]
 const sample = lines.slice(1)
     .map(line => [Math.random(), line])
-    .sort((left, right) => { return left[0] < right[0] ? -1 : 1 })
+    .sort((left, right) => { return left[0] - right[0] })
     .slice(0, parseInt(numLines))
     .map(pair => pair[1])
 fs.writeFileSync(outputFile, header + '\n' + sample.join('\n'))
